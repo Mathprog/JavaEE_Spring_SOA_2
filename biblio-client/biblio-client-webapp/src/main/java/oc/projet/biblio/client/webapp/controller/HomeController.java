069e45665e2dbf6f80f5643479ff0f857c27c6dc
Mathieu Martinez <mathieu-martinez@live.com>
2018-12-24 20:06:43 +0100

    "modifications de la conf. pour connexion à la bdd d'intégration."

--- biblio-client/biblio-client-webapp/src/main/java/oc/projet/biblio/client/webapp/controller/HomeController.java
+++ biblio-client/biblio-client-webapp/src/main/java/oc/projet/biblio/client/webapp/controller/HomeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
     @RequestMapping(value = { "/" }, method = RequestMethod.GET)
     public String homePage(ModelMap model) {
         return "home";
