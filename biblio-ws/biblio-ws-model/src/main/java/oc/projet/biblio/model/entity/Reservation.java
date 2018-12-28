76e8b408682300c3ee7196c6a9dac01680c4cda3
Mathieu Martinez <mathieu-martinez@live.com>
2018-12-28 19:12:40 +0100

    ajout dans la bonne branche.

---
+++ biblio-ws/biblio-ws-model/src/main/java/oc/projet/biblio/model/entity/Reservation.java
@@ -0,0 +1,29 @@
+package oc.projet.biblio.model.entity;
+
+package oc.projet.biblio.model.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public interface Reservation {
+    int getId();
+
+    void setId(int id);
+
+    Ouvrage getOuvrage();
+
+    void setOuvrage(Ouvrage ouvrage);
+
+    Usager getUsager();
+
+    void setUsager(Usager usager);
+
+    LocalDateTime getDateReservation();
+
+    void setDateReservation(LocalDateTime dateReservation);
+
+    LocalDate getDateLimite();
+
+    void setDateLimite(LocalDate dateLimite);
+}
+
